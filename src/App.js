8221957040bddcf3b6e5df2f35d7c732e336957c
ja2z <93806968+ja2z@users.noreply.github.com>
2021-11-14 15:38:43 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useRef } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import addTreemapModule from "highcharts/modules/treemap";
+import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
+//npm install highcharts highcharts-react-official
+
+client.config.configureEditorPanel([
+  { name: "source", type: "element" },
+  { name: "dimension", type: "column", source: "source", allowMultiple: true },
+  { name: "measures", type: "column", source: "source", allowMultiple: true },
+]);
+
+addTreemapModule(Highcharts);
 
 function App() {
+  const config = useConfig();
+  const sigmaData = useElementData(config.source);
+  const ref = useRef();
+  const options = useMemo(() => {
+    const dimensions = config.dimension;
+    const measures = config.measures;
+
+    // transform sigmaData --> treemap data
+    let dataMap = [];
+    const delim = "__";
+    function getParent(i, j) {
+      if (i > 0) {
+        return getId(i - 1, j);
+      } else {
+        return undefined;
+      }
+    }
+    function getId(i, j) {
+      const parent = getParent(i, j);
+      let id = "";
+      if (parent) {
+        id = parent + delim;
+      }
+      id += dimensions[i] + delim + sigmaData[dimensions[i]][j];
+      return id;
+    }
+    if (sigmaData?.[dimensions[0]]) {
+      for (let i = 0; i < dimensions.length; i++) {
+        for (let j = 0; j < sigmaData[dimensions[i]].length; j++) {
+          const name = sigmaData[dimensions[i]][j];
+          const value = sigmaData[measures[i]][j];
+          const id = getId(i, j);
+          const parent = getParent(i, j);
+          const dataPoint = {
+            id,
+            name,
+            value,
+            ...(parent && { parent }),
+          };
+          dataMap[id] = dataPoint;
+        }
+      }
+      // convert object map to array
+      let data = [];
+      let i = 0;
+      for (var key in dataMap) {
+        data[i] = dataMap[key];
+        i++;
+      }
+
+      const options = {
+        plotOptions: {
+          series: {
+            // general options for all series
+          },
+          treemap: {
+            //colorByPoint: true,
+          },
+        },
+        title: {
+          text: undefined,
+        },
+        chart: {
+          height: window.innerHeight,
+          backgroundColor: "transparent",
+        },
+        series: [
+          {
+            levels: [
+              {
+                level: 1,
+                colorByPoint: true,
+                layoutAlgorithm: "sliceAndDice",
+                borderWidth: 6,
+                dataLabels: {
+                  enabled: true,
+                  align: "left",
+                  verticalAlign: "top",
+                  style: {
+                    fontSize: "18px",
+                    fontWeight: "bold",
+                  },
+                },
+              },
+              
+              {
+                level: 2,
+                colorByPoint: true,
+                layoutAlgorithm: "sliceAndDice",
+                borderWidth: 3,
+                dataLabels: {
+                  enabled: true,
+                  align: "right",
+                  verticalAlign: "top",
+                  style: {
+                    fontSize: "14px",
+                    fontWeight: "bold",
+                  },
+                },
+              },
+              
+            ],
+            type: "treemap",
+            allowDrillToNode: true,
+            layoutAlgorithm: "squarified",
+            data: data,
+          },
+        ],
+      };
+      return options;
+    }
+  }, [config, sigmaData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} ref={ref} />
     </div>
   );
 }
